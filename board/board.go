2ed9cd4fd96fd0b3ecf55e89db54e2029343c0ce
Rafael Garcia-Suarez <rafael.garciasuarez@booking.com>
2020-08-01 17:51:26 +0200

    First minimally functional version

--- board/board.go
+++ board/board.go
@@ -5,7 +5,7 @@ import "fmt"
 type Matrix88 [8][8]int
 
 type Coordinates struct {
-	x, y int // 0 to 7
+	X, Y int // 0 to 7
 }
 
 const (
@@ -26,23 +26,39 @@ type Board interface {
 
 func (m *Matrix88) String() string {
 	s := "  a b c d e f g h\n"
+	possibilitiesBlack, possibilitiesWhite := 0, 0
 	for x := 0 ; x < 8 ; x++ {
 		s += fmt.Sprintf("%d", x + 1)
 		for y := 0 ; y < 8 ; y++ {
 			switch m[x][y] {
 			case EMPTY:
-				s += " ."
+				canBlack := m.CanPlayerPlayAt(BLACK, Coordinates{x,y})
+				canWhite := m.CanPlayerPlayAt(WHITE, Coordinates{x,y})
+				if (canBlack) {
+					possibilitiesBlack++
+				}
+				if (canWhite) {
+					possibilitiesWhite++
+				}
+				if (canBlack && canWhite) {
+					s += " ;"
+				} else if (canBlack) {
+					s += " ."
+				} else if (canWhite) {
+					s += " ,"
+				} else {
+					s += " _"
+				}
 			case BLACK:
-				s += " X"
+				s += " \u25CF" // BLACK CIRCLE
 			case WHITE:
-				s += " O"
+				s += " \u25CB" // WHITE CIRCLE
 			}
 		}
 		s += "\n"
 	}
-	// TODO show where players can play and number of possibilities by player
 	black, white := m.CountByPlayer()
-	s += fmt.Sprintf("Black: %02d - White: %02d\n", black, white)
+	s += fmt.Sprintf("Black \u25CF %02d (%d) - White \u25CB %02d (%d)", black, possibilitiesBlack, white, possibilitiesWhite)
 	return s
 }
 
@@ -74,16 +90,81 @@ func (m *Matrix88) PossibilitiesByPlayer() (black, white int) {
 	return
 }
 
+func opponent(color int) int {
+	if color == BLACK {
+		return WHITE
+	} else if color == WHITE {
+		return BLACK
+	} else {
+		return EMPTY
+	}
+}
+
+func (m *Matrix88) disksToFlipInOneDirection(player int, start Coordinates, dx, dy int) []Coordinates {
+	// we already verified that start.X,start.Y is empty
+	x, y := start.X + dx, start.Y + dy
+	var flips []Coordinates
+	enclosed := false
+	opponent := opponent(player)
+	for ; x >= 0 && y >= 0 && x < 8 && y < 8 ; x, y = x + dx, y + dy {
+		if m[x][y] == opponent {
+			flips = append(flips, Coordinates{x, y})
+		} else if m[x][y] == player {
+			enclosed = true
+			break
+		} else {
+			break
+		}
+	}
+	if enclosed {
+		return flips
+	}
+	return nil
+}
+
 func (m *Matrix88) CanPlayerPlayAt(color int, position Coordinates) bool {
-	if m[position.x][position.y] != EMPTY {
+	if m[position.X][position.Y] != EMPTY {
 		return false
 	}
-	// TODO short-circuit - find at least one
-	return true
+	// find at least one direction that works (short-circuit)
+	var flips []Coordinates
+	flips = m.disksToFlipInOneDirection(color, position, -1, -1)
+	if len(flips) > 0 {
+		return true
+	}
+	flips = m.disksToFlipInOneDirection(color, position, -1, 0)
+	if len(flips) > 0 {
+		return true
+	}
+	flips = m.disksToFlipInOneDirection(color, position, -1, 1)
+	if len(flips) > 0 {
+		return true
+	}
+	flips = m.disksToFlipInOneDirection(color, position, 0, -1)
+	if len(flips) > 0 {
+		return true
+	}
+	flips = m.disksToFlipInOneDirection(color, position, 0, 1)
+	if len(flips) > 0 {
+		return true
+	}
+	flips = m.disksToFlipInOneDirection(color, position, 1, -1)
+	if len(flips) > 0 {
+		return true
+	}
+	flips = m.disksToFlipInOneDirection(color, position, 1, 0)
+	if len(flips) > 0 {
+		return true
+	}
+	flips = m.disksToFlipInOneDirection(color, position, 1, 1)
+	if len(flips) > 0 {
+		return true
+	}
+	return false
 }
 
 func (m *Matrix88) IsStable(position Coordinates) bool {
-	if m[position.x][position.y] == EMPTY {
+	if m[position.X][position.Y] == EMPTY {
 		return false
 	}
 	// TODO
@@ -114,8 +195,30 @@ func (m *Matrix88) IsFinal() bool {
 }
 
 func (m *Matrix88) PlayAt(color int, position Coordinates) (Board, error) {
-	// TODO
-	return m, nil
+	if m[position.X][position.Y] != EMPTY {
+		return nil, &BoardError{"Position already occupied", position}
+	}
+	var flips [][]Coordinates = make([][]Coordinates, 8)
+	flips[0] = m.disksToFlipInOneDirection(color, position, -1, -1)
+	flips[1] = m.disksToFlipInOneDirection(color, position, -1, 0)
+	flips[2] = m.disksToFlipInOneDirection(color, position, -1, 1)
+	flips[3] = m.disksToFlipInOneDirection(color, position, 0, -1)
+	flips[4] = m.disksToFlipInOneDirection(color, position, 0, 1)
+	flips[5] = m.disksToFlipInOneDirection(color, position, 1, -1)
+	flips[6] = m.disksToFlipInOneDirection(color, position, 1, 0)
+	flips[7] = m.disksToFlipInOneDirection(color, position, 1, 1)
+	flipped := false
+	for i := 0 ; i < 8 ; i++ {
+		for _, c := range flips[i] {
+			m[c.X][c.Y] = color
+			flipped = true
+		}
+	}
+	if flipped {
+		m[position.X][position.Y] = color
+		return m, nil
+	}
+	return nil, &BoardError{"Cannot play here", position}
 }
 
 func InitialBoard() Board {
--- othellobuddy.go
+++ othellobuddy.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
-import "github.com/rgs/othellobuddy/board"
+import(
+	"fmt"
+	"bufio"
+	"os"
+	"regexp"
+	"github.com/rgs/othellobuddy/board"
+)
 
 // Commands
 // - play at x, y
@@ -12,6 +17,57 @@ import "github.com/rgs/othellobuddy/board"
 
 func main() {
 	fmt.Println("Othello Buddy")
-	fmt.Println("-------------")
-	fmt.Println(board.InitialBoard())
+	curBoard := board.InitialBoard()
+	curPlayer := board.BLACK
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Println("-----------------")
+		fmt.Println(curBoard)
+		if curPlayer == board.BLACK {
+			fmt.Println("Black \u25CF to play")
+		} else {
+			fmt.Println("White \u25CB to play")
+		}
+		fmt.Println("Enter position, or 'exit':")
+		cmd, _ := reader.ReadString('\n')
+		cmd = cmd[:len(cmd)-1]
+		if cmd == "exit" {
+			break
+		}
+		if isPosition, _ := regexp.MatchString("^[a-h][1-8]$", cmd) ; isPosition {
+			var x, y int
+			x = int(cmd[1]) - int('1')
+			y = int(cmd[0]) - int('a')
+			var position board.Coordinates = board.Coordinates{x,y}
+			newBoard, err := curBoard.PlayAt(curPlayer, position)
+			if err != nil {
+				fmt.Println("Error:", err)
+			} else {
+				curBoard = newBoard
+				if curBoard.IsFinal() {
+					fmt.Println("End of game")
+					fmt.Println(curBoard)
+					break
+				}
+				// select new player. Check if we need to pass
+				blackP, whiteP := curBoard.PossibilitiesByPlayer()
+				switch curPlayer {
+				case board.BLACK:
+					if whiteP == 0 {
+						fmt.Println("White \u25CB passes!")
+					} else {
+						curPlayer = board.WHITE
+					}
+				case board.WHITE:
+					if blackP == 0 {
+						fmt.Println("Black \u25CF passes!")
+					} else {
+						curPlayer = board.BLACK
+					}
+				}
+			}
+		} else {
+			fmt.Println("?")
+		}
+	}
 }
